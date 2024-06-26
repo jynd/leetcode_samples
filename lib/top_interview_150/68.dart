f9041075375f09c8243e68d3d37bad10fc42cecc
duy.trinhduc <duy.trinhduc@abs.vn>
2024-06-26 12:25:08 +0700

    done 68

--- lib/top_interview_150/68.dart
+++ lib/top_interview_150/68.dart
@@ -1,32 +1,42 @@
 void main() {
-  var words = ["This", "is", "an", "example", "of", "text", "justification."];
-  int maxWidth = 16;
+  var words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"];
+  int maxWidth = 20;
 
+  /// copilot fixed this code
   print(fullJustify(words, maxWidth));
 }
 
 List<String> fullJustify(List<String> words, int maxWidth) {
   List<String> result = [];
-  int addCount =0;
+  int i = 0;
 
-  if (result.isEmpty) {
-    result.add(words.first);
-    addCount++;
-  }
-  for (int i = 1; i <= words.length - 1; i++) {
-    int lengthWord = words[i].length;
+  while (i < words.length) {
+    int j = i + 1;
+    int lineLength = words[i].length;
 
-    for (int j = addCount - 1; j <= result.length - 1; j++) {
-      int lengthResultItem = result[j].length;
-
-      if (lengthResultItem + lengthWord<= maxWidth) {
-        result[j] = result[j] + ' ' + words[i];
-      } else {
-        result.add(words[i]);
-        addCount++;
-        break;
-      }
+    while (j < words.length && lineLength + words[j].length + (j - i) <= maxWidth) {
+      lineLength += words[j].length;
+      j++;
     }
+
+    int diff = maxWidth - lineLength;
+    int numberOfWords = j - i;
+
+    if (numberOfWords == 1 || j >= words.length) {
+      result.add(words[i] + ' ' * diff);
+    } else {
+      int spaces = diff ~/ (numberOfWords - 1);
+      int extraSpaces = diff % (numberOfWords - 1);
+
+      String line = words[i];
+      for (int k = i + 1; k < j; k++) {
+        line += ' ' * (spaces + (k - i <= extraSpaces ? 1 : 0)) + words[k];
+      }
+      result.add(line);
+    }
+
+    i = j;
   }
+
   return result;
 }
